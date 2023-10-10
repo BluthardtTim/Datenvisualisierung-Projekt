75a77a823b432e6afbc24539b36dc1077567b503
BluthardtTim <tim.bluthardt@hfg.design>
2023-10-10 11:27:11 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,60 +8,56 @@ TEC costa rica, hfg schw. gmuend
 let incomeData;
 let myIncome = [];
 
-let countryData2;
-let myBundesländer = [];
+let consumData;
+let myConsum = [];
 
-function preload(){
+function preload() {
 
     // incomeData = loadTable('data/Deutschland.csv', 'csv', 'header');
 
     incomeData = loadTable('data2/EinkommenEu.csv', 'csv', 'header');
-    // countryData2 = loadTable('data2/komsumpreisindex_eu.csv', 'csv', 'header');
+    consumData = loadTable('data2/konsumpreisindex_eu.csv', 'csv', 'header');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    let i=0;
-    let t=0;
+    let i = 0;
+    let t = 0;
 
-    
-    for (let myRow of incomeData.rows){
+    // Einkommen der Eu Länder
+    for (let myRow of incomeData.rows) {
         let currentCountry2 = new Country();
 
         currentCountry2.myCountryArea = myRow.get('Value');
-        if (currentCountry2.myCountryArea > 60){
+        if (currentCountry2.myCountryArea > 60) {
             currentCountry2.myCountry = myRow.get('LOCATION');
             currentCountry2.myLocation = myRow.get('LOCATION');
-            currentCountry2.mySize = map( currentCountry2.myCountryArea, 17098250,50,  50000,50) ; // [17 098 250,50]
+            currentCountry2.mySize = map(currentCountry2.myCountryArea, 17098250, 50, 50000, 50); // [17 098 250,50]
 
-
-            if ( currentCountry2.myLocation === "DEU") {
-            myIncome [t] = currentCountry2;
-            t++;
+            if (currentCountry2.myLocation === "DEU") {
+                myIncome[t] = currentCountry2;
+                t++;
             }
-
-
         }
     }
 
+    // Consum der Eu Länder
+    for (let myRow of consumData.rows) {
+        let currentCountry = new Country();
 
-    // for (let myRow of countryData2.rows){
-    //     let currentCountry = new Country();
+        currentCountry.myCountryArea = myRow.get('Value');
+        if (currentCountry.myCountryArea > 10000) {
+            currentCountry.myCountry = myRow.get('LOCATION');
+            currentCountry.myCountryISO = myRow.get('TIME');
+            currentCountry.mySize = map(currentCountry.myCountryArea, 17098250, 50, 70000, 50); // [17 098 250,50]
 
-    //     currentCountry.myCountryArea = myRow.get('Value');
-    //     if (currentCountry.myCountryArea > 10000){
-    //         currentCountry.myCountry = myRow.get('LOCATION');
-    //         currentCountry.myCountryISO = myRow.get('TIME');
-    //         currentCountry.mySize = map( currentCountry.myCountryArea, 17098250,50,  70000,50) ; // [17 098 250,50]
+            if (currentCountry.myLocation === "DEU")
+                currentCountry.myColor = color(200, 100, 100);
 
-            // if ( currentCountry.myLocation === "DEU")
-                // currentCountry.myColor = color(200,100,100);
-
-    //         myBundesländer [i] = currentCountry;
-    //         i++;
-    //     }
-    // }
-
+            myConsum[i] = currentCountry;
+            i++;
+        }
+    }
 }
 
 
@@ -69,30 +65,29 @@ function draw() {
     background(41);
 
     let currentX = 20;
-    let currentY = 750;
+    let currentY = 350;
 
-    
-    for (let i = 0; i < myBundesländer.length; i++) {
-        myBundesländer [i].display (currentX, currentY);
-        currentX += myBundesländer[i].myWidth+3;
+
+    for (let i = 0; i < myConsum.length; i++) {
+        myConsum[i].display(currentX, currentY);
+        currentX += myConsum[i].myWidth + 3;
     }
 
 
     currentX = 700;
 
     for (let t = 0; t < myIncome.length; t++) {
-        myIncome [t].display2 (currentX, currentY);
-        currentX += myIncome[t].myWidth+3;
+        myIncome[t].display2(currentX, currentY);
+        currentX += myIncome[t].myWidth + 3;
     }
 
 
-    fill (200);
+    fill(200);
     textSize(18);
-    text ("Einkommen der Deutschen", 20, 300);
-    text ("Lebenshaltungskosten Deutschland", 700, 300);
+    // text ("Einkommen der Deutschen", 20, 300);
+    // text ("Lebenshaltungskosten Deutschland", 700, 300);
     textSize(12);
-    text ("In USD", 20, 315);
-    text("frameRate:   " + Math.round(frameRate()), 20, height-10);
+    text("frameRate:   " + Math.round(frameRate()), 20, height - 10);
 
     // noLoop();
 }
